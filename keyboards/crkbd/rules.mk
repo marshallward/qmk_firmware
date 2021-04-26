f1ebb3e24e282230cb5f8c9af738b31b7ab14d3d
Marshall Ward <marshall.ward@gmail.com>
2021-04-25 21:20:45 -0400

    Add marshall layout (Corne Colemak)
    Adding my layout to the QMK repository.  Mostly a standard Colemak
    layout on a Corne keyboard, with modifiers aligned to the thumbs.

--- keyboards/crkbd/rules.mk
+++ keyboards/crkbd/rules.mk
@@ -9,24 +9,25 @@ MCU = atmega32u4
 #   QMK DFU      qmk-dfu
 #   ATmega32A    bootloadHID
 #   ATmega328P   USBasp
-BOOTLOADER = caterina
+#BOOTLOADER = caterina
+BOOTLOADER = atmel-dfu
 
 # Build Options
 #   change to "no" to disable the options, or define them in the Makefile in
 #   the appropriate keymap folder that will get included automatically
 #
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = no       # Audio control and System control
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = no        # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no        # Commands for debug and configuration
+COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
 RGB_MATRIX_ENABLE = no
 RGB_MATRIX_DRIVER = WS2812
 
@@ -34,7 +35,7 @@ RGB_MATRIX_DRIVER = WS2812
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
 # if firmware size over limit, try this option
-# CFLAGS += -flto
+CFLAGS += -flto
 LAYOUTS = split_3x5_3 split_3x6_3
 
 DEFAULT_FOLDER = crkbd/rev1/legacy
